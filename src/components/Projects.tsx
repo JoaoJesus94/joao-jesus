8c0e736fa66e0834303c043e14e87b3f0dee18d5
Joao Jesus <joaojesus_94@hotmail.com>
2023-02-22 01:04:04 +0000

    style changes on project info

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,62 +1,101 @@
 import { Heading } from '@/components/ui/Heading'
+import { FiGithub, FiExternalLink } from 'react-icons/fi'
+
 import Image from 'next/image'
+import { Link } from '@/components/ui/Link'
+import { cx } from 'class-variance-authority'
 
 const projectsData = [
   {
+    demo: 'https://joaojesus.dev',
     description:
-      'Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    imgUrl: 'https://placekitten.com/300/300',
-    name: 'JoaoJesus.dev',
-    stack: ['html', 'css', 'JavaScript', 'TypeScript'],
+      'text not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    github: 'https://github.com/JoaoJesus94/joao-jesus',
+    imgUrl:
+      'https://res.cloudinary.com/joaojesus/image/upload/v1677024738/JoaoJesus.dev/joaojesus.dev.png',
+    name: 'joaojesus.dev',
+    stack: ['React', 'NextJS', 'TypeScript', 'Tailwindcss'],
   },
   {
+    demo: 'https://joaojesus.dev',
     description:
-      'Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-    imgUrl: 'https://placekitten.com/300/300',
-    name: 'Pokedex',
-    stack: ['html', 'css', 'JavaScript', 'TypeScript'],
+      'text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    github: 'https://github.com/JoaoJesus94/joao-jesus',
+    imgUrl:
+      'https://res.cloudinary.com/joaojesus/image/upload/v1677024738/JoaoJesus.dev/joaojesus.dev.png',
+    name: 'sd.dev',
+    stack: ['React', 'NextJS', 'TypeScript', 'Tailwindcss'],
+  },
+  {
+    demo: 'https://joaojesus.dev',
+    description:
+      'text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    github: 'https://github.com/JoaoJesus94/joao-jesus',
+    imgUrl:
+      'https://res.cloudinary.com/joaojesus/image/upload/v1677024738/JoaoJesus.dev/joaojesus.dev.png',
+    name: 'ass.dev',
+    stack: ['React', 'NextJS', 'TypeScript', 'Tailwindcss'],
   },
 ]
 
 export function Projects() {
   return (
-    <section className='md:text-center'>
-      <Heading className='pb-10' highlight='Projects' text="I've done" />
-      <div className='flex flex-col gap-16'>
-        {projectsData.map(({ name, description, imgUrl, stack }) => (
-          <div key={name} className='flex flex-col md:flex-row gap-6'>
-            <Image
-              className='rounded-lg self-center md:max-w-[300px]'
-              src={imgUrl}
-              width={300}
-              height={300}
-              alt='random cat'
-            />
-            <div className='flex flex-col md:justify-between gap-6 text-start w-full relative'>
-              <div className='flex flex-col font-heading'>
-                <span className='font-medium gradient-heading dark:gradient-heading-dark'>
-                  Project name
-                </span>
-                <h3 className='text-xl font-semibold'>{name}</h3>
-              </div>
-              <p className='md:absolute md:top-0 md:bottom-0 md:-left-20 md:h-fit md:max-h-[172px] md:m-auto p-4 bg-[#d067ff2b] backdrop-blur-sm rounded-sm text-sm'>
-                {description}
-              </p>
-              <div className='flex flex-col font-heading'>
-                <span className='font-medium gradient-heading dark:gradient-heading-dark'>
-                  Tools and Languages
-                </span>
-                <div className='flex flex-row gap-x-6 items-center flex-wrap'>
-                  {stack.map(tech => (
-                    <span key={tech} className='font-medium'>
-                      {tech}
+    <section id='projects' className='md:text-center'>
+      <Heading className='pb-10' highlight='Projects' text="I've worked on" />
+      <div className='flex flex-col gap-20'>
+        {projectsData.map(({ name, description, imgUrl, stack, github, demo }, index) => {
+          const isOdd = !!(index % 2)
+          return (
+            <div
+              key={name}
+              className={cx(
+                'flex flex-col gap-6 opacity-50 hover:opacity-90 transition-opacity ease-in',
+                isOdd ? 'md:flex-row-reverse' : 'md:flex-row'
+              )}
+            >
+              <Image
+                className='rounded-lg self-center max-w-md max-h-[252px] w-full h-full object-cover border border-[#d067ff2b]'
+                src={imgUrl}
+                width={400}
+                height={250}
+                alt='random cat'
+              />
+              <div className='flex flex-col md:justify-between gap-6 text-start w-full relative'>
+                <div className='flex justify-between'>
+                  <div className='flex flex-col font-heading'>
+                    <span className='font-medium gradient-heading dark:gradient-heading-dark'>
+                      Project name
                     </span>
-                  ))}
+                    <h3 className='text-xl font-semibold'>{name}</h3>
+                  </div>
+                  <div className='flex gap-2'>
+                    <Link href={github} target='_blank' rel='noopener noreferrer'>
+                      <FiGithub size={24} />
+                    </Link>
+                    <Link href={demo} target='_blank' rel='noopener noreferrer'>
+                      <FiExternalLink size={24} />
+                    </Link>
+                  </div>
+                </div>
+                <p className='p-4 bg-[#d067ff2b] backdrop-blur-2xl rounded-sm text-sm'>
+                  {description}
+                </p>
+                <div className='flex flex-col font-heading'>
+                  <span className='font-medium gradient-heading dark:gradient-heading-dark'>
+                    Tools and Languages
+                  </span>
+                  <div className='flex flex-row gap-x-6 items-center flex-wrap'>
+                    {stack.map(tech => (
+                      <span key={tech} className='font-medium'>
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </section>
   )
